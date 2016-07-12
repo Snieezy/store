0160a3f457d1c2549aa91f47bcee7603be231245
Snieezy <Snieezy@github.com>
2016-07-13 00:34:41 +0200

    Add spaces

--- lib/services/add_product_to_warehouse.rb
+++ lib/services/add_product_to_warehouse.rb
@@ -2,7 +2,7 @@ module Store
   class AddProductToWarehouse
     def call(wh_id, pr_id, quantity)
       product = FetchProductFromWarehouse.new.call(wh_id, pr_id)
-      product.quantity=(product.quantity + quantity)
+      product.quantity = (product.quantity + quantity)
     end
   end
 end
