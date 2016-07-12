6934b7a784569d631bb8ea8f7dafe8e90ae9ebeb
Snieezy <Snieezy@github.com>
2016-07-12 16:02:42 +0200

    Fix test basket: is not nil

--- spec/unit/basket_spec.rb
+++ spec/unit/basket_spec.rb
@@ -108,7 +108,7 @@ RSpec.describe Store::Basket do
       whouse = Store::CreateWarehouse.new.call
       basket = Store::CreateBasket.new.call
       expect {
-        basket.add_product(whouse, whouse.products[0].id, nil)
+        Store::AddToBasket.new.call(whouse.id, basket.id, 1, nil)
       }.to raise_error(InvalidQuantityError)
     end
 
