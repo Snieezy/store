1e1146e2a630d2e9072095ae550accca482d15c7
Snieezy <Snieezy@github.com>
2016-07-13 01:05:46 +0200

    Fix rename

--- app.rb
+++ app.rb
@@ -4,14 +4,16 @@ require "sinatra/reloader"
 Dir["./lib/**/*.rb"].each{|file| require file}
 
 module Store
+  WAREHOUSE = []
+  BASKET = []
 
   class App < Sinatra::Base
     whouse = CreateWarehouse.new.call
     basket = CreateBasket.new.call
 
     get "/" do
-      @wh_products = FetchProductsFromWarehouse.new.call(whouse.id)
-      @bk_products = FetchProductsFromBasket.new.call(basket.id)
+      @whouse_products = FetchProductsFromWarehouse.new.call(whouse.id)
+      @basket_products = FetchProductsFromBasket.new.call(basket.id)
       @sum_netto = SumBasketNetto.new.call(basket.id)
       @sum_brutto = SumBasketBrutto.new.call(basket.id)
       erb :index
