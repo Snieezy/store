e9d46061e803ae89dd5e9ca23a32297f8425bc0e
Snieezy <snieezy@github.com>
2016-07-13 20:30:02 +0200

    Remove parentheses

--- lib/services/sub_product_from_warehouse.rb
+++ lib/services/sub_product_from_warehouse.rb
@@ -3,7 +3,7 @@ module Store
     def call(warehouse_id, id, quantity)
       product = FetchProductFromWarehouse.new.call(warehouse_id, id)
       raise InvalidQuantityError if product.quantity < quantity
-      product.quantity = (product.quantity - quantity)
+      product.quantity = product.quantity - quantity
     end
   end
 end
