c8503f4098f5acf769ffd5edb628885fbe34ed5a
Snieezy <snieezy@github.com>
2016-07-13 20:40:46 +0200

    Change to ? expression

--- lib/services/fetch_product_from_warehouse.rb
+++ lib/services/fetch_product_from_warehouse.rb
@@ -2,8 +2,7 @@ module Store
   class FetchProductFromWarehouse
     def call(warehouse_id, product_id)
       item = FetchProductsFromWarehouse.new.call(warehouse_id).find{|pr| pr.id == product_id}
-      raise InvalidIDError unless item
-      item
+      item.nil? ? (raise InvalidIDError) : item
     end
   end
 end
