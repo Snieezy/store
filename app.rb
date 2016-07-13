c54de6aa33c3282868dae66844ea478060ed96c5
Snieezy <snieezy@github.com>
2016-07-13 20:14:55 +0200

    Change @ to locals

--- app.rb
+++ app.rb
@@ -13,22 +13,24 @@ module Store
 
     get "/" do
       @warehouse_products = FetchProductsFromWarehouse.new.call(warehouse.id)
-      erb :"warehouse/index"
+      erb :"warehouse/index", locals: { warehouse_products: @warehouse_products }
     end
 
     get "/basket" do
       @basket_products = FetchProductsFromBasket.new.call(basket.id)
       @sum_netto = SumBasketNetto.new.call(basket.id)
       @sum_brutto = SumBasketBrutto.new.call(basket.id)
-      erb :"basket/basket"
+      erb :"basket/basket", locals: { basket_products: @basket_products, sum_netto: @sum_netto, sum_brutto: @sum_brutto }
     end
 
     get "/:id" do |id|
       @product = FetchProductFromWarehouse.new.call(warehouse.id, id.to_i)
-      erb :"product/product"
+      @result = ""
+      erb :"product/product", locals: { product: @product, result: @result }
     end
 
     post "/:id" do
+      @result = ""
       @product = FetchProductFromWarehouse.new.call(warehouse.id, params[:id].to_i)
       begin
         Store::AddToBasket.new.call(warehouse.id, basket.id, params[:id].to_i, params[:amount].to_i)
@@ -38,15 +40,17 @@ module Store
       rescue InvalidQuantityError
         @result = "wrong amount"
       end
-      erb :"product/product"
+      erb :"product/product", locals: { product: @product, result: @result }
     end
 
     get "/:id/delete" do
       @product = FetchProductFromWarehouse.new.call(warehouse.id, params[:id].to_i)
-      erb :"product/delete"
+      @result = ""
+      erb :"product/delete", locals: { product: @product, result: @result }
     end
 
     post "/:id/delete" do
+      @result = ""
       begin
         @product = FetchProductFromWarehouse.new.call(warehouse.id, params[:id].to_i)
         Store::SubProductFromBasket.new.call(warehouse.id, basket.id, params[:id].to_i, params[:amount].to_i)
@@ -56,7 +60,7 @@ module Store
       rescue InvalidQuantityError
         @result = "wrong amount"
       end
-      erb :"product/delete"
+      erb :"product/delete", locals: { product: @product, result: @result }
     end
   end
 end
--- sql/create.sql
+++ sql/create.sql
@@ -4,12 +4,19 @@ CREATE TABLE product
   name Varchar(50) NOT NULL,
   price MONEY NOT NULL,
   vat NUMERIC(2,2) NOT NULL,
-  quantity INTEGER NOT NULL CHECK (quantity > 0),
+  quantity INTEGER NOT NULL CHECK (quantity >= 0),
+  warehouse_id SERIAL REFERENCES warehouse ON DELETE CASCADE NOT NULL
   CHECK (vat < 1.0),
   CHECK (vat >= 0)
 );
 
 CREATE TABLE basket
 (
-  id SERIAL PRIMARY KEY
+  id SERIAL PRIMARY KEY,
+  product_id SERIAL REFERENCES product ON DELETE CASCADE
+);
+
+CREATE TABLE warehouse
+(
+  id SERIAL PRIMARY KEY,
 );
